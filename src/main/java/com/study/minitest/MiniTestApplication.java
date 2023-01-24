5d970a4735212c824dbc95f043ebdda23dbf4489
vvsungho <vvsungho@gmail.com>
2023-01-24 20:11:45 +0900

    initial commit

---
+++ src/main/java/com/study/minitest/MiniTestApplication.java
@@ -0,0 +1,13 @@
+package com.study.minitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/minitest/MiniTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.minitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
